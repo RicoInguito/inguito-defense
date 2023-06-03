77318d88fe56894a5066584a3602d2c048b0860d
Elizar22 <130819270+Elizar22@users.noreply.github.com>
2023-06-03 14:27:54 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,15 +1,38 @@
 <?php
- include 'main.php';
 
-class Dbname extends Database
+include 'main.php';
 
+class Province extends Database 
 {
-    public function db():string
+    public $tblname = "province";
+
+    public function createTable()
     {
-        $dis ="CREATE DATABASE IF NOT EXISTS $this->dbName";
-        
-        return $this->conn->query($dis);
+        $tbl = "CREATE TABLE IF NOT EXISTS $this->tblname(
+            id int primary key auto_increment,
+            street varchar(199) not null,
+            barangay varchar(199) not null,
+            municipality varchar(199) not null
+             )";
+    
+             $this->conn->query($tbl);
+            
+    }
+    
+    public function getAll()
+    {
+        $s = "SELECT * FROM $this->tblname";
+         return  $this->conn->query($s);
+    }
+
+
+    public function insert($street, $barangay, $municipality)
+    {
+        $in = "INSERT INTO $this->tblname(street, barangay, municipality)
+        values(NULL, '$street','$barangay', '$municipality)";
+         
+       return $this->conn->query($in);
+       var_dump($this->conn->error);
     }
 }
-
-?>
+$province=new Province();
